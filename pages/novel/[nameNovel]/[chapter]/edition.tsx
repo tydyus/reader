433f9d98e267088b00406d06253720b4459f522c
Tydyus <tydyus@gmail.com>
2021-08-24 09:45:54 +0200

    add route edition

---
+++ pages/novel/[nameNovel]/[chapter]/edition.tsx
@@ -0,0 +1,15 @@
+import {useRouter} from 'next/router';
+interface Props {}
+
+const Chapter:React.FC<Props> = props =>{
+    const router = useRouter();
+    return (
+        <div className="novelChapterPage">
+            <h1>Novel {router.query.nameNovel}, chapter {router.query.chapter}</h1>
+            <h2>Edition</h2>
+        </div>
+    )
+
+}
+
+export default Chapter;
